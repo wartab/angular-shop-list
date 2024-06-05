cce323b0746760480834f7a3b2bc61003590ad9f
Vincent <vincent@lycoops.be>
2024-06-05 10:44:55 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
+import {MonBeauTitreComponent} from "./mon-beau-titre.component";
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
     ],
-    declarations: [AppComponent],
+    declarations: [AppComponent, MonBeauTitreComponent],
     bootstrap: [AppComponent],
 })
 export class AppModule {
