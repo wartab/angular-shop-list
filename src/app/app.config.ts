c1d275aa6618f414ac88119e753bc7e3965fb38f
Vincent <vincent@lycoops.be>
2025-11-12 12:56:16 +0100

    .

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,9 @@
-import {ApplicationConfig} from "@angular/core";
+import {ApplicationConfig, provideZonelessChangeDetection} from "@angular/core";
 import {provideAnimations} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideAnimations()],
+    providers: [
+        provideZonelessChangeDetection(),
+        provideAnimations()
+    ],
 };
